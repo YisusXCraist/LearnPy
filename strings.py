8ee1ff46c4e0a9ca5d66123e42abf31800ecd302
Yisus <yisusxcraist@gmail.com>
2023-03-31 18:19:05 -0600

    Strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+### Strings ###
+
+my_string = "Hello World"
+my_other_string = 'Hello World 2'
+
+print(len(my_string)) # 11
+print(len(my_other_string)) # 13
+
+print(my_string + my_other_string) # Hello WorldHello World 2
