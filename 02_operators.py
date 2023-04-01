b51f14cf6a9b9edeec38fcc58eecc2dc8b8ce444
Yisus <yisusxcraist@gmail.com>
2023-03-31 18:14:30 -0600

    continue

---
+++ 02_operators.py
@@ -0,0 +1,40 @@
+### Operadores ###
+
+# Operadores aritméticos
+# +, -, *, /, //, %, **
+# suma, resta, multiplicación, división, división entera, módulo, potencia
+print(3+4) # Suma
+print(3-4) # Resta
+print(3*4) # Multiplicación
+print(3/4) # División
+print(3//4) # División entera
+print(3%4) # Módulo
+print(3**4) # Potencia
+
+print("Hola" + " " + "Mundo") # Concatenación de strings
+
+# Operadores de comparación
+
+# ==, !=, >, <, >=, <=
+# igual, diferente, mayor que, menor que, mayor o igual que, menor o igual que
+print(3 == 4) # Igual
+print(3 != 4) # Diferente
+print(3 > 4) # Mayor que
+print(3 < 4) # Menor que
+print(3 >= 4) # Mayor o igual que
+print(3 <= 4) # Menor o igual que
+
+print("Hola" > "Python") # Mayor que
+print("Hola" < "Python") # Menor que
+print("Hola" >= "Python") # Mayor o igual que
+print("Hola" <= "Python") # Menor o igual que
+print("Hola" == "Python") # Igual
+print("Hola" != "Python") # Diferente
+#con los strings se compara el valor ASCII de cada caracter
+ 
+# Operadores lógicos
+print(3 > 4 and 4 < 5) # and
+# True and True = True
+print(3 > 4 or 4 < 5) # or
+# True or False = True
+print(not 3 > 4) # not
