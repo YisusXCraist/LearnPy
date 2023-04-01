294997c9092b10f8d104e93be53f692d06df1557
Yisus <yisusxcraist@gmail.com>
2023-03-31 21:09:11 -0600

    lists

--- 04_lists.py
+++ 04_lists.py
@@ -38,3 +38,29 @@ print(name)
 
 print(my_list + my_other_list) #concatenation
 
+my_list = "Hola Python"
+print(my_list)
+print(type(my_list))
+
+my_list = list("Hola Python asas")
+print(my_list) 
+
+my_other_list.append("Ouroboros")
+print(my_other_list) #append adds an element to the end of the list
+
+my_other_list.insert(0, "Python") 
+print(my_other_list) #insert adds an element to the list in the position specified
+
+my_other_list.remove("Python")
+print(my_other_list) #remove removes the first element that matches the value specified
+
+my_list.pop() #pop removes the last element of the list
+print(my_list)
+print(my_list.pop(0)) #pop can also remove an element in a specific position
+my_pop = my_list.pop(0)
+print(type(my_pop)) #pop returns the element that was removed
+
+#YisusxCraist!
+
+del my_list[2] #del removes an element in a specific position
+print(my_list)
