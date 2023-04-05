da53727f1674c088f9974fc3ad29517c74c0076c
Yisus <yisusxcraist@gmail.com>
2023-04-04 18:56:48 -0600

    new dictiionaries file

---
+++ 07_dictionaries.py
@@ -0,0 +1,21 @@
+### Dictionaries ###
+
+my_dict = dict() #dict() creates an empty dictionary
+my_other_dict = {} #creates an empty dictionary
+
+print(type(my_dict)) #prints the type of the dictionary
+print(type(my_other_dict)) #prints the type of the dictionary
+
+my_other_dict = {"Name":"Yisus", "Surname":"xCraist", "Age":28, "Height":1.65} #creates a dictionary with the elements specified, relation between keys and values
+print(my_other_dict) #prints the dictionary
+
+my_dict = {
+    "Name":"Yisus",
+    "Surname":"xCraist",
+    "Age":28,
+    "Height":1.65,
+    1 : "One",
+    "Lenguajes" : {"Kotlin", "Swift", "Python"}
+}
+
+print(my_dict) #prints the dictionary
