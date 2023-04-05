7a380b7df9d1536145292528b1738dfe26f1e28d
Yisus <yisusxcraist@gmail.com>
2023-04-04 18:38:55 -0600

    Sets complete, init dictionaries

--- 06_sets.py
+++ 06_sets.py
@@ -35,3 +35,17 @@ print(len(my_other_set)) #prints the length of the set
 
 del my_other_set #deletes the set
 # print(my_other_set) #prints an error because the set doesn't exist
+
+my_set = {"Yisus", "xCraist", 1.65, 28} #creates a set with the elements specified
+my_list = list(my_set) #converts the set to a list
+print(my_list) #prints the list 
+print(my_list[0]) #prints the first element of the list
+#This type of conversion is useful when you need to access the elements of the set, but risking the order of the elements as a set is unordered
+
+my_other_set = {"Kotlin", "Swift", "Python"}
+my_new_set = my_set.union(my_other_set) #creates a new set with the union of the two sets
+print(my_new_set) #prints the new set
+
+print(my_new_set.union({"Java", "C++"})) #prints the new set with the union of the new set and the new elements
+
+print(my_new_set.difference(my_set)) #prints the elements of the new set that are not in the first set
