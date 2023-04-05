d92b22eb7ae7b789201f8f46ca317107f74a4c32
Yisus <yisusxcraist@gmail.com>
2023-04-04 22:20:40 -0600

    Erros Handling

--- 11_classes.py
+++ 11_classes.py
@@ -8,24 +8,30 @@ print(MyEmptyPerson) # This will print the class definition.
 print(MyEmptyPerson()) # This will print the class instance.
 
 class MyPerson:
-    def __init__(self, name, surname, age, alias = "The Dude"):
+    def __init__(self, name, surname, age, dui, alias = "The Dude"): 
         self.name = name
         self.surname = surname
         self.age = age
-        self.alias = alias
+        self.alias = alias # This is a public attribute. It can be accessed inside or outside the class.
+        self.__dui = dui # This is a private attribute. It can only be accessed inside the class.
     
     def walk(self):
         print(f"{self.name} is walking. Let's go {self.alias}")
 
-my_person = MyPerson("John", "Doe", 35)
+    def get_dui(self):
+        return(self.__dui)
+
+my_person = MyPerson("John", "Doe", 35, 223)
 print(my_person.name) # This will print the class instance.
 my_person.walk() # This will print the class instance.
 
-my_other_person = MyPerson("Jane", "Doe", 30, "The Lady")
+my_other_person = MyPerson("Jane", "Doe", 30, 4323, "The Lady")
 print(my_other_person.name) # This will print the class instance.
 my_other_person.walk() # This will print the class instance.
 my_other_person.age = 31
 print(my_other_person.age) # This will print the class instance.
+#print(my_other_person.__dui) # This will print the class instance. This will throw an error because the attribute is private.
+print(my_other_person.get_dui()) # This will print the class instance. This will work because the attribute is private but we have a getter method.
 
 
 
---
+++ 12_exceptions.py
@@ -0,0 +1,3 @@
+#### Exception Handling ####
+# Exceptions are errors that occur during execution of a program
+# Exceptions are handled with try and except statements
