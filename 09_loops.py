0840e3c511ee73813a4e3812e74c143afb192d42
Yisus <yisusxcraist@gmail.com>
2023-04-04 20:44:09 -0600

    loops

--- 09_loops.py
+++ 09_loops.py
@@ -1,2 +1,15 @@
 ### Loops ###
 
+#>>>>> While loop
+
+# While loop is used to execute a block of code repeatedly until a given condition is satisfied. 
+#And when the condition becomes false, the line immediately after the loop in the program is executed. 
+#The while loop falls under the category of indefinite iteration.
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 1
+# Output: 0 1 2 3 4 5 6 7 8 9
+# Note: The condition is checked before the loop is executed.
