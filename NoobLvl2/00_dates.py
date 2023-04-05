bb3f194353c01b1217b6c1b4d7c700745bd4809c
Yisus <yisusxcraist@gmail.com>
2023-04-04 23:32:22 -0600

    Commit 100

--- NoobLvl2/00_dates.py
+++ NoobLvl2/00_dates.py
@@ -2,4 +2,34 @@
 # Python has a module named datetime to work with dates as date objects.
 # Import the datetime module and display the current date:
 
-import datetime
+from datetime import datetime
+
+now = datetime.now()
+
+# The datetime.now() method returns the current date and time.
+
+print(now) # 2020-05-01 15:10:00.000000
+print(now.year) # 2020
+print(now.month) # 5
+print(now.day) # 1
+print(now.hour) # 15
+print(now.minute) # 10
+print(now.second) # 0
+print(now.microsecond) # 0
+
+timestamp = now.timestamp()
+print(timestamp) # 1588332200.0
+
+
+year_2023 = datetime(2023, 4, 4)
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.microsecond)
+
+print_date(year_2023) # 2023 4 4 0 0 0 0
