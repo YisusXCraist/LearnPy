b206c5d3b19178c26e3b59cbd0124fc8525c2efb
Yisus <yisusxcraist@gmail.com>
2023-04-06 11:49:32 -0600

    1

--- NoobLvl2/04_higher_order_functions.py
+++ NoobLvl2/04_higher_order_functions.py
@@ -14,3 +14,26 @@
 #
 # print(sum_or_multiply_two_values(5, 6, sum_two_values))
 
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_one(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_one(5, 6, sum_one))
+print(sum_two_values_and_one(5, 6, sum_five))
+
+### Closures ###
+# A closure is a function that remembers the values from the enclosing lexical scope even when the program flow is not in the enclosing scope.
+# A closure is a function that returns another function.
+
+def sum_ten():
+    def add(value):
+        return value + 10
+    return add
+
+add_closure = sum_ten()
+print(add_closure(5))
