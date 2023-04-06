2020e9846e20a3a0d31a648fd136be5f985411e2
Yisus <yisusxcraist@gmail.com>
2023-04-06 11:39:02 -0600

    HOF

--- NoobLvl2/03_lambdas.py
+++ NoobLvl2/03_lambdas.py
@@ -4,3 +4,25 @@
 # Syntax:
 # lambda arguments : expression
 #
+
+# Example:
+# x = lambda a : a + 10
+# print(x(5))
+
+# Example:
+# x = lambda a, b : a * b
+# print(x(5, 6))
+
+sum_two_values = lambda first_value, second_value: first_value + second_value
+print(sum_two_values(5, 6))
+
+multiply_two_values = lambda first_value, second_value: first_value * second_value
+print(multiply_two_values(5, 6))
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+sum_three_values_lambda = sum_three_values(5)
+print(sum_three_values_lambda(5, 6))
+# print(sum_three_values(5)(5, 6))
+
---
+++ NoobLvl2/04_higher_order_functions.py
@@ -0,0 +1,16 @@
+### Higher Order Functions ###
+# A function that takes a function as an argument is called a higher order function.
+# A function that returns a function is also called a higher order function.
+#
+# Example:
+# def sum_two_values(first_value, second_value):
+#     return first_value + second_value
+#
+# def multiply_two_values(first_value, second_value):
+#     return first_value * second_value
+#
+# def sum_or_multiply_two_values(first_value, second_value, function):
+#     return function(first_value, second_value)
+#
+# print(sum_or_multiply_two_values(5, 6, sum_two_values))
+
