63cfe40c849e5a190ddedad07feb2c2d699a73bb
Yisus <yisusxcraist@gmail.com>
2023-04-06 18:53:44 -0600

    fff

---
+++ NoobLvl2/08_packages_management.py
@@ -0,0 +1,19 @@
+### Python Package Manager (pip) ###
+#
+# pip is a package manager for Python packages, or modules if you like.
+#
+# What is a Package?
+# A package contains all the files you need for a module.
+# Modules are Python code libraries you can include in your project.
+#
+
+# PIP https://pypi.org/project/pip/
+
+import numpy # pip install numpy
+
+print(numpy.version.version)
+
+
+import pandas # pip install pandas
+
+import requests # pip install requests
